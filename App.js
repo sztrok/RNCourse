c2c8c51c8bd1c05dda999bbe2ef79d808d098e6f
Kamil Sakowicz <kamil.sakowicz@comarch.com>
2024-11-28 15:26:56 +0100

    pressable and styling

--- App.js
+++ App.js
@@ -7,12 +7,17 @@ export default function App() {
 
     const [courseGoals, setCourseGoals] = useState([]);
 
-
     function addGoalHandler(enteredGoalText) {
         setCourseGoals(currentCourseGoals => [...currentCourseGoals, {
             text: enteredGoalText,
             id: Math.random().toString()
         }]);
+    }
+
+    function deleteGoalHandler(id) {
+        setCourseGoals(currentCourseGoals => {
+            return currentCourseGoals.filter(goal => goal.id !== id);
+        })
     }
 
     return (
@@ -22,7 +27,13 @@ export default function App() {
               <FlatList
                 data={courseGoals}
                 renderItem={(itemData) => {
-                    return <GoalItem text={itemData.item.text}/>;
+                    return (
+                      <GoalItem
+                        text={itemData.item.text}
+                        id={itemData.item.id}
+                        onDeleteItem={deleteGoalHandler}
+                      />
+                    );
                 }}
                 keyExtractor={(item, index) => {
                     return item.id;
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,16 @@
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, Pressable} from "react-native";
 
 function GoalItem(props) {
     return (
       <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+          <Pressable
+            android_ripple={{color: 'red'}}
+            onPress={props.onDeleteItem.bind(this, props.id)}
+            style={({pressed}) => pressed && styles.pressedItem}
+          >
+              <Text style={styles.goalText}>{props.text}</Text>
+          </Pressable>
+      </View>
     );
 }
 
@@ -13,10 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
-    }, goalText: {
+    },
+    pressedItem: {
+        opacity: 0.5,
+    },
+    goalText: {
         color: 'white',
+        padding: 8,
     }
 })
