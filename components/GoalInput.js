688bfdec43f7a0a123d899eeca9426dbd86a65c3
sztrok <kamil.sakowicz@comarch.com>
2024-12-02 15:21:51 +0100

    test change2

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -13,7 +13,6 @@ function GoalInput(props) {
         setEnteredGoalText('')
     }
 
-
     return (
       <Modal visible={props.visible} animationType={"slide"} style={styles.modal}>
           <View style={styles.inputContainer}>
